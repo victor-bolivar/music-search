378e21f78a3225599b54a65c58b142dbaebbea03
Victor <vbolivar124@gmail.com>
2020-07-19 20:16:37 -0500

    Updated ItunesInformation Class : add __init__, __str__, list_results

--- music_search.py
+++ music_search.py
@@ -13,19 +13,45 @@ def timer(function):
 	return wrapped
 
 
-class ItunesInformation():
+class ItunesInformation:
+	def __init__(self, song):
+		self.song = song
+		self.data = 'NO DATA FOUND'
+
+	def __str__(self):
+		return 'object with information for the song : '+self.song.upper()
+
+	def encode(self, string):
+		return string.replace(' ', '+')
 
 	@timer
-	def get_data(self, song):
+	def get_data(self):
+		''''returns a dictionary with the data for 5 results related to
+		a given song, which name 'song' must be encoded (i.e. De+Musica+Ligera)'''
+
 		base_url = 'https://itunes.apple.com/search?'
 		parameters = {}
-		parameters['term'] = song
+		parameters['term'] = self.encode(self.song)
 		parameters['country'] = 'US'
+		parameters['media'] = 'music'
+		parameters['entity'] = 'musicTrack'
+		parameters['limit'] = '5'
 
-		res =requests.get(base_url, params=parameters)
-		print(res.json())
+		res = requests.get(base_url, params=parameters)
+		self.data = res.json()
+
+	def data_generator(self):
+		for i in range(5):
+			yield self.data['results'][i]['trackId'], self.data['results'][i]['trackPrice'], self.data['results'][i]['trackViewUrl']
+
+	def list_results(self):
+		''' returns a list of dictionaries with trackId,trackPrice and trackViewUrl'''
+		results = [{'trackId':trackId, 'trackPrice':trackPrice, 'trackViewUrl':trackViewUrl} for trackId,trackPrice,trackViewUrl in self.data_generator()]
+		results = sorted(results, key=lambda result : float(result['trackPrice']), reverse=True)
+		return results
 
 if __name__ == '__main__':
 
-	iTunes = ItunesInformation()
-	iTunes.get_data('YMCA')
+	iTunes = ItunesInformation('de musica ligera')
+	iTunes.get_data()
+	print(iTunes.list_results())
